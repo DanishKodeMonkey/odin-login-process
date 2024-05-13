61a95aa7775338a1f528af619668921470deb758
danishkodemonkey <humberjini@hotmail.com>
2024-05-13 11:49:17 +0200

    Sign in works

--- app.js
+++ app.js
@@ -62,8 +62,16 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 // get relevant views, assign URLs
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) =>
+    res.render(
+        'index',
+        // Send the user object to the index if available
+        { user: req.user }
+    )
+);
 app.get('/sign-up', (req, res) => res.render('sign-up-form'));
+
+/// Signing up ///
 
 // app POST for sign up form so we can add users to our database
 // REMEMBER in a real scenario, make sure to sanitize credentials
@@ -83,4 +91,77 @@ app.post('/sign-up', async (req, res, next) => {
     }
 });
 
+/// Logging in ///
+
+// Set up a LocalStrategy auth process using passport middleware
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            // Find the user
+            const user = await User.findOne({ username: username });
+            // user not found
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username' });
+            }
+            // match user.password against given password
+            if (user.password !== password) {
+                // password incorrect
+                return done(null, false, { message: 'Incorrect password' });
+            }
+            // Bot user and password match, success!
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    })
+);
+
+/// Handling cookies ///
+
+// Passport will create cookies on the users browser with their authentication to allow them
+// stay logged in during their visit
+
+// use seriealize and deserialise user to determine what information passport should look for when
+// creating and decoding the cookies
+
+// create
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+// decode
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});
+
+// Now passport will receive the user object on login and store its id in the session data.
+// Upon any other request, if a matching session is found for that request, passport
+// will find that retrieve the id stored, and use it to query our database for the user.
+// finally, the user object will be attached to req.user.
+
+// now add a post to the app listening to the index page /log-in action
+// and run authentication using passport.authenticate
+app.post(
+    '/log-in',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/',
+    })
+);
+
+// Finally, allow the user to log out again, by creating a route
+app.get('/log-out', (req, res, next) => {
+    // this will use a method to clear the cookie
+    req.logout(err => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
 app.listen(3000, () => console.log('App listening on port 3000!'));
